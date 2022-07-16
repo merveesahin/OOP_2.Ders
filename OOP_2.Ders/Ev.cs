ca9cf55a1fbfedb99ed4b4fd31a1f9d01d1f0aca
merve <merve.nur14@outlook.com>
2022-07-16 18:34:48 +0300

    Add project files.

---
+++ OOP_2.Ders/Ev.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_2.Ders
+{
+    public class Ev
+    {
+        private int kapiNumarasi;
+
+        public int odaSayisi { get; set; }
+        public int KapiNumarasi
+        {
+            get
+            {
+                return kapiNumarasi;
+            }
+            set
+            {
+                if (value > 100)
+                {
+                    Console.WriteLine("Kapı Numarası 100'den büyük olamaz !!!");
+                }
+                else
+                {
+                    this.kapiNumarasi = value;
+                }
+            }
+        }
+    }
+}
---
+++ OOP_2.Ders/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP_2.Ders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ev ev = new Ev();
+            ev.odaSayisi = 5;
+            ev.KapiNumarasi = 511;
+
+            Console.WriteLine("Kapı Numarası {0}", ev.KapiNumarasi);
+            Console.WriteLine("Oda Sayısı {0}", ev.odaSayisi);
+           
+        }
+    }
+}
